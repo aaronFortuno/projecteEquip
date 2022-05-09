84ba9c16b49b1054b4e06152ae310d05d6e44220
aaronFortuno <101064283+aaronFortuno@users.noreply.github.com>
2022-05-09 18:58:52 +0200

    inicialització arxiu

---
+++ java/Projecte.java
@@ -0,0 +1,16 @@
+package Projecte;
+
+import java.util.Scanner;
+
+/**
+*
+* @author @oscardefez @xonitu @aaronFortuno
+*/
+
+public class Projecte {
+  
+  public static void main (String[] args) {
+    
+    
+  }
+}
