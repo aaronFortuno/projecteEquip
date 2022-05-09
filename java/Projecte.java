0690311273118e380c3f11fe9a07af88ca1e5a23
aaronFortuno <101064283+aaronFortuno@users.noreply.github.com>
2022-05-09 19:27:42 +0200

    declaració constants

--- java/Projecte.java
+++ java/Projecte.java
@@ -10,6 +10,24 @@ import java.util.Scanner;
 public class Projecte {
   
   // declaració constants
+  public static final int ID_MIN = 10;
+  public static final int ID_MAX = 999;
+  
+  public static final int PLACES_MIN = 1;
+  public static final int PLACES_MAX = 99;
+  
+  public static final String PARLA_NO = "no";
+  public static final String PARLA_SI = "sí";
+  
+  public static final String TIPUS_0 = "habitació compartida";
+  public static final String TIPUS_1 = "habitació";
+  public static final String TIPUS_2 = "habitatge sencer";
+  public static final String TIPUS_3 = "sala comunitària, local habilitat";
+  
+  public static final int TELF_MIN = 111111111;
+  public static final int TELF_MAX = 999999999;
+  
+  public static final int INTENTS = 3;
   
   
   public static void main (String[] args) {
