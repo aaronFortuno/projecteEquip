5b18f2ad28691bdb9f4e77383b2cd81582957807
aaronFortuno <101064283+aaronFortuno@users.noreply.github.com>
2022-05-11 12:24:57 +0200

    modularity changes, code saving and optimization

--- java/Projecte.java
+++ java/Projecte.java
@@ -1,71 +1,39 @@
-package Projecte;
+package Projecte3;
 
 import java.util.Scanner;
-
 /**
-*
-* @author @oscardefez @xonitu @aaronFortuno
-*/
-
-public class Projecte {
-  
-  // declaració constants
-  public static final int ID_MIN = 10;
-  public static final int ID_MAX = 999;
-  
-  public static final int PLACES_MIN = 1;
-  public static final int PLACES_MAX = 99;
-  
-  public static final String PARLA_NO = "no";
-  public static final String PARLA_SI = "sí";
-  
-  public static final String TIPUS_0 = "habitació compartida";
-  public static final String TIPUS_1 = "habitació";
-  public static final String TIPUS_2 = "habitatge sencer";
-  public static final String TIPUS_3 = "sala comunitària, local habilitat";
-  
-  public static final int TELF_MIN = 111111111;
-  public static final int TELF_MAX = 999999999;
-  
-  public static final int INTENTS = 3;
-  public static final int SORTIR = -1;
-  
-  
-  public static void main (String[] args) {
+ *
+ * @author @oscardefez @xonitu @aaronFortuno
+ */
+public class P3_FortunoR {
     
-    // declaració i inicialització variables
-    Scanner sc = new Scanner(System.in);
-    int id, places, parla, tipus, telefon;
-    boolean valid, confirmaSortida;
-    String textParla, textTipus;
+    // declaració constants
+    public static final int ID_MIN = 10;
+    public static final int ID_MAX = 999;
     
-    // inicialització interfície usuari
-    System.out.println("Benvingut/da al programa de gestió de famílies d'acollida.\nIntrodueix [-1] en qualsevol moment per finalitzar el programa");
+    public static final int PLACES_MIN = 1;
+    public static final int PLACES_MAX = 99;
     
-    // INSTRUCCIONS INICIALS
-    //
+    public static final String PARLA_NO = "no";
+    public static final String PARLA_SI = "sí";
     
-    // bucle programa
-    do {
-      System.out.println("Introdueix la ID de la família:");
-      valid = sc.hasNextInt(); // verifica si tipus correcte
-      if (valid) {
-        id = sc.nextInt();
-        if (id >= ID_MIN && id <= ID_MAX) { // verifica si dins del rang de dades
-          // valid=true; <<< no cal aquesta línia perquè en el moment en què hem verificat si és del tipus correcte aquest booleà passa a ser true
-
-          // System.out.println("Introdueix ID"); <<< es demana en iniciar el do/while
-
-          // SEGUIM PROGRAMA
-          System.out.println("Heu introduït la ID: " + id); // feedback id
-          /*
-          SEGUIR AQUÏ DINS
-          
-          
-          */
-          
-        } else if (id == SORTIR) { // exit
-          System.out.println("Estàs segur de voler sortir? [1] Sí / [0] No"); // rutina confirmació sortida
+    public static final String TIPUS_0 = "habitació compartida";
+    public static final String TIPUS_1 = "habitació";
+    public static final String TIPUS_2 = "habitatge sencer";
+    public static final String TIPUS_3 = "sala comunitària, local habilitat";
+    
+    public static final int TELF_MIN = 111111111;
+    public static final int TELF_MAX = 999999999;
+    
+    public static final int INTENTS = 3;
+    public static final int SORTIR = -1;
+    
+    static Scanner sc = new Scanner (System.in);
+    static boolean valid;
+    static int confirmaSortida;
+            
+    static void sortir() {
+        System.out.println("Estàs segur de voler sortir? [1] Sí / [0] No"); // rutina confirmació sortida
           do {
             valid = sc.hasNextInt();
             if (valid) {
@@ -74,6 +42,7 @@ public class Projecte {
                 System.out.println("Gràcies per utilitzar el programa d'introducció de famílies");
               } else if (confirmaSortida == 0) { // no sortir, torna a l'inici
                 System.out.println("Has triat seguir utilitzant el programa");
+                families();
               } else { // no valor 0 o 1
                 System.out.println("No has introduït un valor vàlid (1 o 0), torneu-ho a intentar");
                 valid = false; // torna a demanar
@@ -84,17 +53,42 @@ public class Projecte {
               valid = false; // torna a demanar
             }
           } while (!valid);
-        } else {
-          valid = false; // torna a demanar entrada dades si valor fora del rang
-          sc.nextLine(); // buida buffer
-        }
-      } else { // torna a demanar entrada dada si tipus incorrecte
-        sc.nextLine(); // buida buffer
-      }
-      
-    } while (!valid);
-  }
+    }
+    
+    static void families() {
+        // declaració i inicialització variables
+        int id, places, parla, tipus, telefon;
+        String textParla, textTipus;
+        
+        // inicialització interfície usuari
+        System.out.println("Benvingut/da al programa de gestió de famílies d'acollida.\nIntrodueix [-1] en qualsevol moment per finalitzar el programa.");
+        
+        // INSTRUCCIONS INICIALS
+        //
+        
+        // bucle programa
+        do {
+            System.out.println("Introdueix la ID de la família [" + ID_MIN + "-" + ID_MAX + "]");
+            valid = sc.hasNextInt(); // verifica si tipus correcte
+            if (valid) {
+                id = sc.nextInt();
+                if (id >= ID_MIN && id <= ID_MAX) { // verifica si dins del rang de dades
+                    System.out.println("Id: " + id);
+                } else if (id == SORTIR) {
+                    sortir();
+                } else {
+                    System.out.println("No has introduït un valor dins del rang, torna-ho a intentar");
+                    valid = false;
+                    sc.nextLine();
+                }
+            } else {
+                System.out.println("No has introduït un nombre enter, torna-ho a intentar");
+                sc.nextLine();
+            }
+        } while (!valid);
+    }
+    public static void main (String[] args) {
+        
+        families();
+    }
 }
-
-
-//for (i=0;i<quant_families;i++);
