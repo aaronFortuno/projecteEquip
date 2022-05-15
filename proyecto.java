2aa7f3766d1c37cd8c1a57b09370db6aa9fd58cc
xonitu <104739531+xonitu@users.noreply.github.com>
2022-05-15 21:19:08 +0200

    Update proyecto.java

--- proyecto.java
+++ proyecto.java
@@ -249,7 +249,20 @@ public class P3_DefezR {
         }
         
         // PART TONI ampliació 1
-        
+         System.out.println("Vols saber qui acolleix varies? 0=NO 1=SI");
+                int vols_saber = 0;
+                opcioCorrecta = scan.hasNextInt();
+                vols_saber = scan.nextInt();
+                if (places[q] >= 2 && vols_saber == RESPOSTA_SI) {
+                    System.out.println(id[q] + "\t" + places[q] + "\t" + res_ru + "\t" + tipus_hab + "\t"
+                            + telefon[q]);
+                }
+                if (vols_saber == RESPOSTA_NO) {
+                    System.out.println("Bye's");
+                }
+               scan.nextLine();
+            }    
+//TONI aqui acabaría la filtrasion.
         
         // PART AARÓN ampliació 2
         
